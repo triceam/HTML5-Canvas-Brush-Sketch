88392decd131516e4e8d141b627ccd17f9dd76fe
Tomek Maszkowski <tomazy@go2.pl>
2012-02-27 23:46:19 +0800

    Removes Math.pow() calls.

--- src/js/trigonometry.js
+++ src/js/trigonometry.js
@@ -3,6 +3,6 @@ var Trig = {
 		
 		var dx = point2.x - point1.x;
 		var dy = point2.y - point1.y;
-		return Math.sqrt( Math.pow( dx, 2 ) + Math.pow( dy, 2 ) );	
+		return Math.sqrt( dx * dx + dy * dy );
 	},
 }
